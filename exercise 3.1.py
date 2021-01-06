07eb6424b032ed8314cb9994f9ba0ca57ddec9c0
arunkumarsugu <76736478+arunkumarsugu@users.noreply.github.com>
2021-01-06 14:25:10 +0530

    Add files via upload

---
+++ exercise 3.1.py
@@ -0,0 +1,12 @@
+"""Write a function named right_justify
+that takes a string named s as a parameter
+and prints the string with enough leading spaces
+so that the last letter of the string is in column 70 of the display"""
+
+def right_justify(s):
+    required_space = 70 - len(s)
+    print(' '*required_space + s)
+
+
+right_justify('arun')
+
